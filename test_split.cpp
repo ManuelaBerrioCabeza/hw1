f4e8c756ccacc1dea257eaaa413482cdf322e4d0
Manuela Berrio <berrio@usc.edu>
2025-01-31 08:05:57 +0000

    finished part 1 :)

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,50 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
 
 int main(int argc, char* argv[])
 {
+  Node* node1 = new Node {9, NULL};
+  Node* node2 = new Node {12, NULL};
+  Node* node3 = new Node {23, NULL};
+  Node* node4 = new Node{6, NULL};
 
+  node1->next = node2;
+  node2->next = node3;
+  node3->next = node4;
+
+  Node* testList = node1;
+  Node* odds = NULL;
+  Node* evens= NULL;
+
+  split(testList, odds, evens);
+
+  if (testList != NULL){
+    return 1;
+  }
+
+  if (odds==NULL || odds->value !=9 || odds->next ==NULL || odds->next->value !=23|| odds->next->next != NULL){
+    return 2;
+  }
+
+  if (evens==NULL || evens->value !=12 || evens->next ==NULL || evens->next->value !=6|| evens->next->next != NULL){
+    return 3;
+  }
+  
+
+  while (odds != NULL){
+    Node* temp= odds;
+    odds = odds->next;
+    delete temp;
+
+  }
+  while (evens != NULL){
+    Node* temp= evens;
+    evens = evens->next;
+    delete temp;
+  }
+
+
+  return 0;
 }
